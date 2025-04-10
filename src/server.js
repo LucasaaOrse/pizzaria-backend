45ecb9d5d0cd522ff5b6da535a74d0a29cb8eea7
LUCAS TEIXEIRA ORSE <Z501886@corp.clarobr>
2025-04-10 19:22:59 -0300

    Primeiro commit

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const fileUpload = require('express-fileupload')
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const cors = require('cors')
 app.use(cors({
