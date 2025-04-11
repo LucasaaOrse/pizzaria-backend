97f749822d8d80bd619d06fce5a355d057d21671
LUCAS TEIXEIRA ORSE <Z501886@corp.clarobr>
2025-04-11 13:02:59 -0300

    Primeiro commit

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,18 @@
+const path = require('path');
+
 module.exports = {
   development: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './migrations'
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     }
   },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './migrations'
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     }
   }
 };
