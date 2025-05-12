0a739fb31e151bd853f8f230f100af0e06e59c1e
LUCAS TEIXEIRA ORSE <Z501886@corp.clarobr>
2025-05-12 13:43:10 -0300

    feat: adiciona rotas de criação, ajuste de estoque e listagem de ingredientes

--- src/routes/ingredients/IngredientsRoutes.js
+++ src/routes/ingredients/IngredientsRoutes.js
@@ -12,11 +12,11 @@ module.exports = (db) => {
     await IngredientsController.create(req, res, db)
   }),
 
-  router.put('/:id/add', async (req, res) => {
+  router.patch('/:id/add', async (req, res) => {
     await IngredientsController.addStock(req, res, db)
   })
 
-  router.put('/:id/remove', async (req, res) => {
+  router.patch('/:id/remove', async (req, res) => {
     await IngredientsController.removeStock(req, res, db)
   })
 
