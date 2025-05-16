4022dbbf95862a49e2bfbba9b95e8a5f89a31ca9
LUCAS TEIXEIRA ORSE <Z501886@corp.clarobr>
2025-05-16 12:18:52 -0300

    Atualização

--- src/routes/stock/stockRoutes.js
+++ src/routes/stock/stockRoutes.js
@@ -1,18 +1,37 @@
 // routes/stockRoutes.js
 const express = require('express');
 const StockController = require('../../controllers/stock/stockController');
-module.exports = (db) => {
-  const router = express.Router();
+const router = express.Router();
 
-  router.get('/',      (req, res) => StockController.index(req, res, db));
-  router.get('/types', (req, res) => StockController.types(req, res, db));
-  router.get('/low',   (req, res) => StockController.lowStock(req, res, db));
-  router.post('/',     (req, res) => StockController.create(req, res, db));
-  router.get('/:id',   (req, res) => StockController.show(req, res, db));
-  router.put('/:id',   (req, res) => StockController.update(req, res, db));
-  router.delete('/:id',(req, res) => StockController.delete(req, res, db));
-  router.post('/bulk-add',    (req, res) => StockController.bulkAdd(req, res, db));
-  router.post('/bulk-remove', (req, res) => StockController.bulkRemove(req, res, db));
+module.exports = (db) => {
+
+  router.get('/', async (req, res) => {
+    await StockController.index(req, res, db)
+  } );
+  router.get('/types', async (req, res) => {
+    await StockController.types(req, res, db)
+  } );
+  router.get('/low', async  (req, res) => {
+    await StockController.lowStock(req, res, db)
+  } );
+  router.post('/', async    (req, res) => {
+    await StockController.create(req, res, db)
+  } );
+  router.get('/:id',  async (req, res) => {
+    StockController.show(req, res, db)
+  } );
+  router.put('/:id',  async (req, res) => {
+    await StockController.update(req, res, db)
+  } );
+  router.delete('/:id', async (req, res) => {
+    await StockController.delete(req, res, db)
+  } );
+  router.post('/bulk-add', async   (req, res) => {
+    await StockController.bulkAdd(req, res, db)
+  } );
+  router.post('/bulk-remove', async (req, res) => {
+    await StockController.bulkRemove(req, res, db)
+  } );
 
   return router;
 };
