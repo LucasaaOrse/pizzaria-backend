196ee4a22cdffe7a20ee5e9662b1c9424db062dc
LUCAS TEIXEIRA ORSE <Z501886@corp.clarobr>
2025-05-16 16:31:36 -0300

    Atualização

--- src/routes/types/typesRoutes.js
+++ src/routes/types/typesRoutes.js
@@ -3,12 +3,16 @@ const router = express.Router();
 
 const TypeController = require("../../controllers/types/TypesController");
 
-router.get("/types", async (req, res) => {
+module.exports = (db) => {
+  router.get("/types", async (req, res) => {
   await TypeController.index(req, res, db)
-});
-router.post("/types", async (req, res) => {
-  await TypeController.create(req, res, db)
-});
-router.delete("/types/:id", async (req, res) => {
-  await TypeController.delete(req, res, db)
-});
+  });
+  router.post("/types", async (req, res) => {
+    await TypeController.create(req, res, db)
+  });
+  router.delete("/types/:id", async (req, res) => {
+    await TypeController.delete(req, res, db)
+  });
+
+}
+
